6a01ab4a6420778615b07261ebb583284a09e775
敛凝 <register@mail.majie.me>
2024-09-26 18:40:03 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -69,12 +69,72 @@ function performSearch() {
                 case 'google':
                     defaultSearchUrl = "https://www.google.com/";
                     break;
-                    case 'weibo':
-                        defaultSearchUrl = "https://weibo.com/";
-                        break;
-                        case 'douban':
-                            defaultSearchUrl = "https://www.douban.com/";
-                            break;
+                case 'weibo':
+                    defaultSearchUrl = "https://weibo.com/";
+                    break;
+                case 'douban':
+                    defaultSearchUrl = "https://www.douban.com/";
+                    break;
+                // case 'wangpan':
+                //     defaultSearchUrl = "https://s.quark.cn/";
+                //     break;
+                case 'baidutupian':
+                    defaultSearchUrl = "https://image.baidu.com/";
+                    break;
+                case 'bilibili':
+                    defaultSearchUrl = "https://www.bilibili.com/";
+                    break;
+                case 'tieba':
+                    defaultSearchUrl = "https://tieba.baidu.com/";
+                    break;
+                case 'music':
+                    defaultSearchUrl = "https://tools.liumingye.cn/music/#/";
+                    break;
+                case 'baike':
+                    defaultSearchUrl = "https://baike.baidu.com/";
+                    break;
+                case 'yingshi':
+                    defaultSearchUrl = "https://www.freeok.vip/";
+                    break;
+                case 'wangpan':
+                    defaultSearchUrl = "https://www.dashengpan.com/";
+                    break;
+                case 'amap':
+                    defaultSearchUrl = "https://www.amap.com/";
+                    break;
+                case 'github':
+                    defaultSearchUrl = "https://github.com/";
+                    break;
+                case 'douyin':
+                    defaultSearchUrl = "https://www.douyin.com/discover";
+                    break;
+                case 'kuaishou':
+                    defaultSearchUrl = "https://www.kuaishou.com/brilliant";
+                    break;
+                case 'xiachufang':
+                    defaultSearchUrl = "https://www.xiachufang.com/";
+                    break;
+                case 'fanyi':
+                    defaultSearchUrl = "https://fanyi.baidu.com/";
+                    break;
+                case 'cnki':
+                    defaultSearchUrl = "https://www.cnki.net/";
+                    break;
+                case 'weipu':
+                    defaultSearchUrl = "https://www.cqvip.com/";
+                    break;
+                case 'wanfang':
+                    defaultSearchUrl = "https://www.wanfangdata.com.cn/";
+                    break;
+                case 'chaoxing':
+                    defaultSearchUrl = "https://qikan.chaoxing.com/";
+                    break;
+                case 'Google Scholar':
+                    defaultSearchUrl = "https://scholar.google.com/";
+                    break;
+                case 'baiduxueshu':
+                    defaultSearchUrl = "https://xueshu.baidu.com/";
+                    break;
             }
             window.open(defaultSearchUrl, '_blank');
         });
@@ -105,18 +165,74 @@ function performSearch() {
                 case 'google':
                     searchUrl = "https://www.google.com/search?q=" + encodeURIComponent(query);
                     break;
-                    case 'weibo':
-                        searchUrl = "https://s.weibo.com/weibo?q=" + encodeURIComponent(query);
-                        break;
-                    case 'douban':
-                        searchUrl = "https://www.douban.com/search?q=" + encodeURIComponent(query);
-                        break;
+                case 'weibo':
+                    searchUrl = "https://s.weibo.com/weibo?q=" + encodeURIComponent(query);
+                    break;
+                case 'douban':
+                    searchUrl = "https://www.douban.com/search?q=" + encodeURIComponent(query);
+                    break;
+                case 'baidutupian':
+                    searchUrl = "https://image.baidu.com/search/index?tn=baiduimage&word=" + encodeURIComponent(query);
+                    break;
+                case 'bilibili':
+                    searchUrl = "https://search.bilibili.com/video?keyword=" + encodeURIComponent(query);
+                    break;
+                case 'tieba':
+                    searchUrl = "https://tieba.baidu.com/f/search/res?ie=utf-8&qw=" + encodeURIComponent(query);
+                    break;
+                case 'music':
+                    searchUrl = "https://tools.liumingye.cn/music/#/search/M/song/" + encodeURIComponent(query);
+                    break;
+                case 'baike':
+                    searchUrl = "https://baike.baidu.com/search?enc=utf8&word=" + encodeURIComponent(query);
+                    break;
+                case 'yingshi':
+                    searchUrl = "https://www.freeok.vip/so1so/-------------.html?wd=" + encodeURIComponent(query);
+                    break;
+                case 'wangpan':
+                    searchUrl = "https://www.dashengpan.com/#/main/search?keyword=" + encodeURIComponent(query);
+                    break;
+                case 'amap':
+                    searchUrl = "https://www.amap.com/search?query=" + encodeURIComponent(query);
+                    break;
+                case 'github':
+                    searchUrl = "https://github.com/search?q=" + encodeURIComponent(query) + "&type=repositories";
+                    break;
+                case 'douyin':
+                    searchUrl = "https://www.douyin.com/search/" + encodeURIComponent(query) + "?type=general";
+                    break;
+                case 'kuaishou':
+                    searchUrl = "https://www.kuaishou.com/search/video?searchKey=" + encodeURIComponent(query);
+                    break;
+                case 'xiachufang':
+                    searchUrl = "https://www.xiachufang.com/search/?keyword=" + encodeURIComponent(query);
+                    break;
+                case 'fanyi':
+                    searchUrl = "https://fanyi.baidu.com/mtpe-individual/multimodal?query=" + encodeURIComponent(query) + "&lang=auto2auto";
+                    break;
+                case 'cnki':
+                    searchUrl = "https://kns.cnki.net/kns8s/defaultresult/index?korder=SU&kw=" + encodeURIComponent(query);
+                    break;
+                case 'weipu':
+                    searchUrl = "https://www.cqvip.com/search?k=" + encodeURIComponent(query);
+                    break;
+                case 'wanfang':
+                    searchUrl = "https://s.wanfangdata.com.cn/paper?q=" + encodeURIComponent(query);
+                    break;
+                case 'chaoxing':
+                    searchUrl = "https://qikan.chaoxing.com/searchjour?sw=" + encodeURIComponent(query) + "&topsearch=0&size=100";
+                    break;
+                case 'Google Scholar':
+                    searchUrl = "https://scholar.google.com/scholar?hl=zh-CN&q=" + encodeURIComponent(query);
+                    break;
+                case 'baiduxueshu':
+                    searchUrl = "https://xueshu.baidu.com/s?wd=" + encodeURIComponent(query) + "&rsv_bp=0&tn=SE_baiduxueshu_c1gjeupa&rsv_spt=3&ie=utf-8&f=3&rsv_sug2=1&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&rsp=1";
+                    break;
             }
             window.open(searchUrl, '_blank');
         });
     }
 }
-
 
 // 监听键盘事件，当用户按下Enter键时执行搜索
 document.addEventListener('DOMContentLoaded', function () {
@@ -156,3 +272,29 @@ function showCategory(categoryId) {
     var selectedCategory = document.getElementById(categoryId);
     selectedCategory.classList.add('active');
 }
+
+
+
+
+
+// ------------------------------------------------------------
+// function selectEngine(engine) {
+//     console.log("Selected engine: " + engine);
+//     // 这里可以添加更多选择搜索引擎后的逻辑
+// }
+
+function toggleMoreTop() {
+    var moreOptions = document.getElementById('moreOptionsTop');
+    moreOptions.style.display = moreOptions.style.display === 'flex' ? 'none' : 'flex';
+}
+function toggleMoreBottom() {
+    var moreOptions = document.getElementById('moreOptionsBottom');
+    moreOptions.style.display = moreOptions.style.display === 'flex' ? 'none' : 'flex';
+}
+
+
+
+
+
+
+// ppppppppppppppppppp

44f30953260c09ad4c6e9e90f2de0e01ea5061af
敛凝 <179696391+majiie@users.noreply.github.com>
2025-01-12 14:00:07 +0800

    time

--- scripts/index.js
+++ scripts/index.js
@@ -153,6 +153,77 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+{/* <script>Time */}
+    const colElms = [];
+
+    function getTimeStr(date = new Date()) {
+        // 返回小时和分钟
+        return [date.getHours(), date.getMinutes()]
+            .map((item) => item.toString().padStart(2, "0"))
+            .join("");
+    }
+
+    function createCol() {
+        const createEl = (cls) => {
+            const div = document.createElement("div");
+            div.classList.add(cls);
+            return div;
+        };
+        const [col, flip, flipNext, flipCurr, next, curr] = ["col", "flip", "next", "curr", "next", "curr"].map(
+            (cls) => createEl(cls)
+        );
+        flip.append(flipNext, flipCurr);
+        col.append(flip, next, curr);
+        time.append(col);
+        return {
+            toggleActive: () => flip.classList.toggle("active"),
+            getCurr: () => curr.dataset.t,
+            setCurr: (t) => [flipCurr, curr].forEach((el) => (el.dataset.t = t)),
+            setNext: (t) => [flipNext, next].forEach((el) => (el.dataset.t = t)),
+        };
+    }
+
+    for (let i = 0; i < 4; i++) {
+        colElms.push(createCol());
+    }
+
+    const timeStr = getTimeStr();
+    colElms.forEach(({ setCurr }, i) => {
+        setCurr(timeStr[i]);
+    });
+
+    let lastMin = new Date().getMinutes();
+    function updateTime() {
+        let m = new Date().getMinutes();
+        if (m === lastMin) {
+            return;
+        }
+        lastMin = m;
+        const currStr = getTimeStr();
+        colElms.forEach(({ toggleActive, getCurr, setCurr, setNext }, i) => {
+            var currTxt = getCurr();
+            setNext(currStr[i]);
+            if (currTxt !== currStr[i]) {
+                toggleActive();
+                setTimeout(() => {
+                    toggleActive();
+                    setCurr(currStr[i]);
+                }, 500);
+            }
+        });
+    }
+
+    function run() {
+        updateTime();
+        setTimeout(() => {
+            run();
+        }, 1000 / 60);
+    }
+
+    run();
+{/* </script> */}
+
+
 // search
 // 定义分类和对应的搜索引擎
 const categories = {

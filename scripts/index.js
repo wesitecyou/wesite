c1bb2fc902dad927d2ba5c3c7ecccf5e05717b0d
敛凝 <179696391+majiie@users.noreply.github.com>
2025-01-31 18:48:42 +0800

    add mobi-toggle-toc button

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,6 @@ toggleColorLinks.forEach(link => {
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.querySelector(".toggle-toc");
     const sidebarToc = document.querySelector(".sidebar-toc");
-    // const leftTopLogo = document.querySelector(".left-top-logo");
 
     let isHidden = false;
 
@@ -27,111 +26,42 @@ document.addEventListener("DOMContentLoaded", () => {
             // 显示目录
             sidebarToc.style.position = 'relative';
             sidebarToc.classList.remove("hidden");
-            // leftTopLogo.classList.remove("hidden");
             toggleButton.textContent = "←";
         } else {
             // 隐藏目录
             sidebarToc.style.position = 'absolute';
             sidebarToc.classList.add("hidden");
-            // leftTopLogo.classList.add("hidden");
             toggleButton.textContent = "→";
         }
         isHidden = !isHidden;
     });
 });
+// mobi-sidebar
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButtonMobi = document.querySelector(".mobi-toggle-toc");
+    const sidebarTocMobi = document.querySelector(".sidebar-toc");
 
-// // Time
-//     const colElms = [];
+    let isHiddenMobi = true;
 
-//     function getTimeStr(date = new Date()) {
-//         // 返回小时和分钟
-//         return [date.getHours(), date.getMinutes()]
-//             .map((item) => item.toString().padStart(2, "0"))
-//             .join("");
-//     }
+    toggleButtonMobi.addEventListener("click", () => {
+        if (isHiddenMobi) {
+                        // 显示目录
+                        sidebarTocMobi.style.display = 'none';
+                        sidebarTocMobi.classList.add("hidden");
+                        toggleButtonMobi.textContent = "→";
+                        
 
-//     function createCol() {
-//         const createEl = (cls) => {
-//             const div = document.createElement("div");
-//             div.classList.add(cls);
-//             return div;
-//         };
-//         const [col, flip, flipNext, flipCurr, next, curr] = ["col", "flip", "next", "curr", "next", "curr"].map(
-//             (cls) => createEl(cls)
-//         );
-//         flip.append(flipNext, flipCurr);
-//         col.append(flip, next, curr);
-//         time.append(col);
-//         return {
-//             toggleActive: () => flip.classList.toggle("active"),
-//             getCurr: () => curr.dataset.t,
-//             setCurr: (t) => [flipCurr, curr].forEach((el) => (el.dataset.t = t)),
-//             setNext: (t) => [flipNext, next].forEach((el) => (el.dataset.t = t)),
-//         };
-//     }
+            
+        } else {
+                        // 隐藏目录
+                        sidebarTocMobi.style.display = 'block';
+                        sidebarTocMobi.classList.remove("hidden");
+                        toggleButtonMobi.textContent = "←";
+        }
+        isHiddenMobi = !isHiddenMobi;
+    });
+});
 
-//     for (let i = 0; i < 4; i++) {
-//         colElms.push(createCol());
-//     }
-
-//     const timeStr = getTimeStr();
-//     colElms.forEach(({ setCurr }, i) => {
-//         setCurr(timeStr[i]);
-//     });
-
-//     let lastMin = new Date().getMinutes();
-//     function updateTime() {
-//         let m = new Date().getMinutes();
-//         if (m === lastMin) {
-//             return;
-//         }
-//         lastMin = m;
-//         const currStr = getTimeStr();
-//         colElms.forEach(({ toggleActive, getCurr, setCurr, setNext }, i) => {
-//             var currTxt = getCurr();
-//             setNext(currStr[i]);
-//             if (currTxt !== currStr[i]) {
-//                 toggleActive();
-//                 setTimeout(() => {
-//                     toggleActive();
-//                     setCurr(currStr[i]);
-//                 }, 500);
-//             }
-//         });
-//     }
-
-//     function run() {
-//         updateTime();
-//         setTimeout(() => {
-//             run();
-//         }, 1000 / 60);
-//     }
-
-//     run();
-
-// // Date
-// function updateDate() {
-//     const now = new Date();
-
-//     // 获取年月日
-//     const year = now.getFullYear();
-//     const month = String(now.getMonth() + 1).padStart(2, '0'); // 补零
-//     const day = String(now.getDate()).padStart(2, '0'); // 补零
-
-//     // 获取星期
-//     const options = { weekday: 'long' };
-//     const weekday = now.toLocaleDateString('zh-CN', options);
-
-//     // 拼接年月日和星期
-//     const formattedTime = `${year}年${month}月${day}日 ${weekday}`;
-//     document.getElementById('currentDate').textContent = formattedTime;
-// }
-
-// // 初始化显示
-// updateDate();
-
-// // 每秒更新一次
-// setInterval(updateDate, 1000);
 
 // search
 // 定义分类和对应的搜索引擎
